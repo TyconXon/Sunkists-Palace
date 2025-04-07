8b21a40f34179b8834a246591b691045e34fbb4d
Boxel <128452230+BoxelLogica@users.noreply.github.com>
2025-04-07 07:54:15 -0700

    Added In-Progress Services for Notifications

---
+++ SERVICE/index.js
@@ -0,0 +1,21 @@
+const check = () => {
+	if (!('serviceWorker' in navigator)) {
+		throw new Error('Service Worker Unsupported.')
+	}
+	else if (!('PushManager' in window)) {
+		throw new Error('Service Worker Unsupported.')
+	}
+	else {
+		console.log('Service Worker and Push Supported!')
+	}
+}
+
+const registerServiceWorker = async () => {
+	const swRegistration = await navigator.serviceWorker.register('notificationWorker.js');
+	return swRegistration;
+}
+const main = async () => {
+	check();
+	const swRegistration = await registerServiceWorker();
+}
+main();
